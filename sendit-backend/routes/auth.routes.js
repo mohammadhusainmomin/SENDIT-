3a7a89ad4a3b74f5f66787a8cc21f022c526bd2c
MohammadHusainMomin <mohammadhusainmsn@gmail.com>
2025-12-27 15:06:46 +0530

    Fix Node 22 wildcard OPTIONS crash

--- sendit-backend/routes/auth.routes.js
+++ sendit-backend/routes/auth.routes.js
@@ -4,9 +4,8 @@ import {
   loginUser,
   googleAuth
 } from "../controllers/auth.controller.js";
-import cors from "cors";
+
 const router = express.Router();
-router.options("*", cors());
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
